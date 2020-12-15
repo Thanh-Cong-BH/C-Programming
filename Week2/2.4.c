c64779ab7d4f0c9bb043e4f7600e491ad57aaf9e
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-15 09:37:57 +0700

    Create 2.4.c

---
+++ Week2/2.4.c
@@ -0,0 +1,9 @@
+/*Bui Huu Thanh Cong - ICT 02-K65 */
+/*This is my second program in C */
+
+#include <stdio.h>
+main()
+{
+   printf("Welcome to C\n");
+   printf("Programming Introduction.\n");
+}
