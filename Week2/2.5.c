2d5b1bd471bb9febcdc709a6d975e5bbf2456ff3
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-15 09:49:48 +0700

    Create 2.5.c

---
+++ Week2/2.5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int i,j;
+for (i=1; i<=30; i++)
+ { printf("*"); }
+ printf("\n\nMy name is Thanh Cong");
+ printf("\nNice to meet you");
+ printf("\nTry your best for a nice job today!\n\n");
+ for (j=1; j<=30; j++)
+    { printf("*"); }
+}
