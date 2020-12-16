72475ce4f422a26d4bfa7667c5073d87173c639f
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-16 20:44:51 +0700

    Create 3.1.c

---
+++ Week3/3.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+
+{
+    printf("SIZE OF SOME BASIC DATA TYPES\n\n");
+    printf("int: %d\n", sizeof(int));
+    printf("long: %d\n", sizeof(long));
+    printf("short: %d\n", sizeof(short));
+    printf("float: %d\n", sizeof(float));
+    printf("double: %d\n", sizeof(double));
+    printf("char: %d\n", sizeof(char));
+}
