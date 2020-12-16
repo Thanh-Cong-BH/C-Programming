db85f807736241f370f46bfa45e0877ba90d0ec3
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-16 10:22:10 +0700

    Update 2.7.c

--- Week2/2.7.c
+++ Week2/2.7.c
@@ -5,6 +5,6 @@ int main()
     printf("#include <stdio.h>\n");
     printf("int main()\n\n");
     printf("{\n");
-    printf("printf("The person sitting next to me is Le Minh Duc");\n");
+    printf("printf(\"The person sitting next to me is Le Minh Duc\\n\");\n");
     printf("}");
 }
