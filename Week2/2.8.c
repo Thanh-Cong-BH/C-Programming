96d965b7b822d32c1af2ad2c96b49e2d504ac64d
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-16 10:19:07 +0700

    Create 2.8.c

---
+++ Week2/2.8.c
@@ -0,0 +1,14 @@
+while (not logged in)
+  { if (power off) turn on }
+  
+log in
+set up/choose working directory
+run Emacs to write and save a program
+run gcc to compile
+if (error(s) exist)
+  { run Emacs
+    edit the file with corresponding error(s)
+    save file
+    run gcc to compile
+  }
+run executable program
