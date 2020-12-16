6b76d8c2b1a1896996213ece944a70b4712f27fb
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-16 10:10:46 +0700

    Create 2.7.c

---
+++ Week2/2.7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+
+{
+    printf("#include <stdio.h>\n");
+    printf("int main()\n\n");
+    printf("{\n");
+    printf("printf("The person sitting next to me is Le Minh Duc");\n");
+    printf("}");
+}
