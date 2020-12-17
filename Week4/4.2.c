57f4ecc6c00f7b0fee03ec19a73642a6028f5a28
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-17 20:15:55 +0700

    Create 4.2.c

---
+++ Week4/4.2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+
+{
+    printf("SOME BASIC TYPES' LIMITS:\n\n");
+    printf("    int: %d to %d\n", INT_MIN, INT_MAX);
+    printf("    long: %ld to %ld\n", LONG_MIN, LONG_MAX);
+}
