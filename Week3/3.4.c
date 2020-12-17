000f962bc5cd5e67cb3c82ca05908a6b41980e72
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-17 18:41:01 +0700

    Create 3.4.c

---
+++ Week3/3.4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+
+{
+    int m,n,S;
+    printf("Enter first number: ");
+    scanf("%d", &m);
+    printf("Enter second number: ");
+    scanf("%d", &n);
+    printf("Sum %d+%d=%d", m, n, m+n);
+}
