35817d29b60b3b58134463a1df71cf1f3629ae2f
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-19 20:29:41 +0700

    Create 6.2.c

---
+++ Week6/6.2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+
+{
+   float HDI;
+   printf("Enter compass heading: ");
+   scanf("%f", &HDI);
+   if (HDI<0 || HDI>360)
+      { printf("Inappropriate data. Try again");
+        printf("Enter compass heading: ");
+        scanf("%f", &HDI); }
+   else if (HDI>=0; HDI<90)
+      { printf("North %f degree east", HDI); }
+   else if (HDI>=90; HDI<180)
+      { printf("South %f degree east", 180.0-HDI); }
+   else if (HDI>=180; HDI<270)
+      { printf("South %f degree west", HDI-180.0); }
+   else if (HDI>=270, HDI<=360)
+      { printf("North %f degree west", 360.0-HDI); }
+}
