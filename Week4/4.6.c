d7e8b3161efe59b385b1c23e32875788cbf9d904
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-21 20:25:13 +0700

    Update 4.6.c

--- Week4/4.6.c
+++ Week4/4.6.c
@@ -5,10 +5,7 @@ int main()
    int hrs, wage;
    printf("Enter worked hours this week: ");
    scanf("%d", hrs);
-   if (hrs>=0, hrs<=40) { wage = 25000*hrs; printf("") }
-   else if (hrs>40, hrs<=60) { wage = 25000*40 + 40000*(hrs-40); }
+   if (hrs>=0, hrs<=40) { wage = 25000*hrs; printf("Wage: %d", wage) }
+   else if (hrs>40, hrs<=60) { wage = 25000*40 + 40000*(hrs-40); printf("Wage: %d", wage) }
    else printf("Inappropriate data! Please try again.");
-   
-   
-  
 }
