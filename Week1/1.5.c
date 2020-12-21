5facbdff938e0293c8c8c5162c99a6da032f8923
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-21 19:34:24 +0700

    Create 1.5.c

---
+++ Week1/1.5.c
@@ -0,0 +1,9 @@
+balance = $100;
+newbuy = productprice * quantity;
+balance = balance - newbuy;
+choose a desired product;
+choose desired quantity;
+    if (balance>0);
+      if (want to buy more) { Back to step 1,2 }
+      else stop
+    else return back, stop;
