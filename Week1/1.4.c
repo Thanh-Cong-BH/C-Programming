d33098658ba5c25aa688f6b6dc08e87a2183266d
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2020-12-21 19:24:12 +0700

    Create 1.4.c

---
+++ Week1/1.4.c
@@ -0,0 +1,18 @@
+{
+  if (hot weather)
+    {
+      rain suit, umbrella (in case of rain);
+      if (go to swimming pool)
+        swim suit, swimming goggle, sunglasses (for outdoor pool);
+      if (go to beach)
+        swim suit, goggle, sunglasses, anti-UV suit;
+      if (go to mountainous area)
+        T-shirt, short (daytime);
+        long clothes (nighttime);
+     }
+   if (cold weather)
+    { 
+      rain suit (in case of rain);
+        long clothes, jacket;
+        if (go skiing) { special glasses to avoid glare }
+   
